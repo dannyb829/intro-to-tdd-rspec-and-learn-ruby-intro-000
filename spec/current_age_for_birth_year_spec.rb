515b4adbb7ddfafebaa75e7e0635006873a2ee39
Daniel Brito <dannyb829@gmail.com>
2020-02-11 03:13:02 +0000

    Automatically backed up by Learn

--- spec/current_age_for_birth_year_spec.rb
+++ spec/current_age_for_birth_year_spec.rb
@@ -8,7 +8,7 @@ describe "current_age_for_birth_year method" do
   end
 end
 
-require_relative './current_age_for_birth_year.rb'
+require_relative '../current_age_for_birth_year.rb'
 
 describe "current_age_for_birth_year method" do
   it "returns the age of a person based on the year of birth" do
